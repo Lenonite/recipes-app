e1a6a3182095aaaa443041cd762b19448ef8371e
Lenonite <crisrogerdoski@gmail.com>
2023-05-02 14:40:39 +0630

    new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {RecipeService} from "./service/recipe.service";
+import {Observable} from "rxjs";
+import {Recipe} from "./model/recipe";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'recipes-app';
-}
+export class AppComponent implements OnInit{
+  recipes$!:Observable<Recipe[]>;
+  constructor(private recipeService:RecipeService) {
+
+  }
+
+
+  ngOnInit(): void {
+    this.recipes$=this.recipeService.listAllRecipes();
+
+  }
+  }
---
+++ src/app/service/recipe.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Recipe} from "../model/recipe";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipeService {
+
+  constructor(private http:HttpClient) { }
+
+  public listAllRecipes():Observable<any>{
+    return this.http.get<Recipe[]>("http://localhost:8080/recipe/list-recipes")
+  }
+}
